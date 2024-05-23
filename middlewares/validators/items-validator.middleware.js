1e32bb2020b0a25a995aa49bc6a4f8bd1d1da906
Dongkyun Kim <donkim0122@gmail.com>
2024-05-23 20:07:25 +0900

    Update: key names now use camelCase

--- middlewares/validators/items-validator.middleware.js
+++ middlewares/validators/items-validator.middleware.js
@@ -2,16 +2,14 @@ import Joi from "joi";
 
 const isNotNumber = /[^0-9]/;
 const itemCodeSchema = Joi.object({
-  item_code: Joi.number().integer().min(1).required(),
+  itemCode: Joi.number().integer().min(1).required(),
 }).unknown(true);
 const itemNameSchema = Joi.object({
-  item_name: Joi.string().trim().min(1).max(30).required(),
+  itemName: Joi.string().trim().min(1).max(30).required(),
 }).unknown(true);
 const itemStatSchema = Joi.object({
-  item_stat: {
-    health: Joi.number().optional().integer(),
-    power: Joi.number().optional().integer(),
-  },
+  itemHealth: Joi.number().optional().integer(),
+  itemPower: Joi.number().optional().integer(),
 }).unknown(true);
 const itemEquipSchema = Joi.object({
   equip: Joi.boolean().strict().required(),
@@ -20,14 +18,14 @@ const itemEquipSchema = Joi.object({
 const itemValidatorJoi = {
   itemCodeValidation: async function (req, res, next) {
     const target = {
-      item_code: req.body.item_code || req.params.item_code,
+      itemCode: req.body.itemCode || req.params.itemCode,
     };
 
     const validation = itemCodeSchema.validate(target);
 
     if (validation.error) {
       console.log("itemCodeValidation: ", validation.error.message);
-      let msg = `Invalid item_code: ${target.item_code}`;
+      let msg = `Invalid item_code: ${target.itemCode}`;
       return res.status(400).json({ errorMessage: msg });
     }
 
@@ -38,7 +36,7 @@ const itemValidatorJoi = {
 
     if (validation.error) {
       console.log("itemNameValidation: ", validation.error.message);
-      let msg = `Invalid item_name: ${req.body.item_name}`;
+      let msg = `Invalid item_name: ${req.body.itemName}`;
       return res.status(400).json({ errorMessage: msg });
     }
 
@@ -49,7 +47,7 @@ const itemValidatorJoi = {
 
     if (validation.error) {
       console.log("itemStatValidation: ", validation.error.message);
-      let msg = `Invalid item_stat: ${req.body.item_stat}`;
+      let msg = `Invalid item stat given.`;
       return res.status(400).json({ errorMessage: msg });
     }
 
@@ -68,45 +66,4 @@ const itemValidatorJoi = {
   },
 };
 
-const itemValidator = {
-  itemCodeValidation: (req, res, next) => {
-    const item_code = req.body.item_code || req.params.item_code;
-    if (isNotNumber.test(item_code))
-      return res
-        .status(400)
-        .json({ errorMessage: `Invalid item_code: ${item_code}` });
-    next();
-  },
-  itemNameValiation: (req, res, next) => {
-    const { item_name } = req.body;
-    if (!item_name)
-      return res
-        .status(400)
-        .json({ errorMessage: `Invalid item_name: ${item_name}` });
-    next();
-  },
-  itemStatValidation: (req, res, next) => {
-    const { item_stat } = req.body || { item_stat: {} };
-
-    if (item_stat.health && isNotNumber.test(item_stat.health)) {
-      return res
-        .status(400)
-        .json({ errorMessage: `Invalid health: ${item_stat.health}` });
-    }
-    if (item_stat.power && isNotNumber.test(item_stat.power)) {
-      return res
-        .status(400)
-        .json({ errorMessage: `Invalid power: ${item_stat.power}` });
-    }
-
-    next();
-  },
-  itemEquipValidation: (req, res, next) => {
-    const { equip } = req.body;
-    if (equip === true || equip === false) next();
-    let msg = "Failed: 'equip' of type 'boolean' must be defined in req.body.";
-    return res.status(400).json({ errorMessage: msg });
-  },
-};
-
-export { itemValidator, itemValidatorJoi };
+export { itemValidatorJoi };
