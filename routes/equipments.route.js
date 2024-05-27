54807b339eb8d050216ca49d074d96ab6db2b8f1
Dongkyun Kim <donkim0122@gmail.com>
2024-05-27 12:57:01 +0900

    Feat: view equipments (READ) & equip/unequip item (UPDATE)

--- routes/equipments.route.js
+++ routes/equipments.route.js
@@ -1,10 +1,10 @@
 import express from "express";
 import { prisma } from "../lib/utils/prisma/index.js";
-import { characterValidatorJoi as cv } from "../middlewares/validators/characters-validator.middleware.js";
-import { itemValidatorJoi as iv } from "../middlewares/validators/items-validator.middleware.js";
-import CharacterNotFoundError from "../lib/errors/character-not-found.error.js";
-import ItemNotFoundError from "../lib/errors/item-not-found.error.js";
-import InvalidEquipOperationError from "../lib/errors/invalid-equip-operation.error.js";
+import { Prisma } from "@prisma/client";
+import cv from "../middlewares/validators/characters-validator.middleware.js";
+import ua from "../middlewares/auths/user-authenticator.middleware.js";
+import iv from "../middlewares/validators/items-validator.middleware.js";
+import et from "../lib/errors/error-thrower.js";
 
 const router = express.Router();
 
@@ -16,21 +16,16 @@ router.get(
   cv.characterIdValidation,
   async (req, res, next) => {
     try {
-      const characterId = req.params.characterId;
+      const { characterId } = req.params;
 
-      const character = await prisma.characters.findFirst({
-        // TODO: check equipments table
-        select: {},
-        where: {
-          characterId: characterId,
-        },
+      et.characterChecker(characterId);
+
+      const equipment = await prisma.equipments.findMany({
+        select: { itemCode: true, itemName: true },
+        where: { characterId: characterId },
       });
 
-      if (!character) throw new CharacterNotFoundError();
-
-      return res.status(200).json({
-        data: character.equipped,
-      });
+      return res.status(200).json({ equipment });
     } catch (err) {
       next(err);
     }
@@ -42,40 +37,55 @@ router.get(
  */
 router.put(
   "/equipments/:characterId",
+  ua.authStrict,
   cv.characterIdValidation,
   iv.itemCodeBodyValidation,
+  iv.itemEquipValidation,
   async (req, res, next) => {
     try {
       const characterId = req.params.characterId;
-      const { item_code, equip } = req.body;
+      const { itemCode, equip } = req.body;
 
-      const character = await prisma.characters.findFirst({
-        characterId: characterId,
-      });
+      // error checkers
+      const character = et.characterChecker(characterId);
+      const item = et.itemChecker(itemCode);
+      const { equipment, inventory } = et.equipChecker(
+        characterId,
+        itemCode,
+        equip,
+      );
 
-      if (!character) throw new CharacterNotFoundError();
+      // can equip/unequip
 
-      const item = await prisma.items.findFirst({
-        item_code: item_code,
-      });
+      await prisma.$transaction(
+        async (tx) => {
+          if (equip)
+            tx.equipments.create({
+              data: {
+                characterId: characterId,
+                itemCode: itemCode,
+              },
+            });
+          else
+            tx.equipments.delete({
+              where: { itemCode: itemCode },
+            });
 
-      if (!item) throw new ItemNotFoundError();
+          prisma.inventories.update({
+            where: { characterId: characterId, itemCode: itemCode },
+            data: {
+              count: equip ? item.count - 1 : item.count + 1,
+            },
+          });
+        },
+        { isolationLevel: Prisma.TransactionIsolationLevel.ReadCommitted },
+      );
 
-      if (character.equipped.includes(item._id)) {
-        if (equip) throw new InvalidEquipOperationError();
-        const idx = character.equipped.indexOf(item._id);
-        character.equipped.splice(idx, 1);
-      } else {
-        if (!equip) throw new InvalidEquipOperationError();
-        character.equipped.push(item._id);
-      }
+      // Change Character stat???
+      // character.health += equip ? item.itemStat.health : -item.itemStat.health;
+      // character.power += equip ? item.itemStat.power : -item.itemStat.power;
 
-      character.health += equip ? item.health : -item.health;
-      character.power += equip ? item.power : -item.power;
-
-      await character.save();
-
-      let msg = `${equip ? "E" : "Une"}quipped the item '${item.item_name}'.`;
+      let msg = `${equip ? "E" : "Une"}quipped the item '${item.itemName}'.`;
       return res.status(200).json({ message: msg });
     } catch (err) {
       next(err);
