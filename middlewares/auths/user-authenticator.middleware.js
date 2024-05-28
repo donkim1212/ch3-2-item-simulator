e570ebe9112d494392adca5cce1e9d73bab19d9e
Dongkyun Kim <donkim0122@gmail.com>
2024-05-28 14:50:19 +0900

    Hotfix: user auth to throw errors with proper status code

--- middlewares/auths/user-authenticator.middleware.js
+++ middlewares/auths/user-authenticator.middleware.js
@@ -5,18 +5,30 @@ const userAuthenticator = {
     try {
       const authorization = req.headers.authorization?.split(" ");
 
-      if (!authorization) throw new Error("Sign-in required.");
-      if (authorization[0] !== "Bearer") throw new Error("Invalid token type.");
+      if (!authorization) {
+        const error = new Error("Sign-in required.");
+        error.code = 401;
+        throw error;
+      }
+      if (authorization[0] !== "Bearer") {
+        const error = new Error("Invalid token type.");
+        error.code = 400;
+        throw error;
+      }
 
       jwt.verify(authorization[1], process.env.SECRET, (err, user) => {
-        if (err) throw Error(err.message);
+        if (err) {
+          const error = new Error(err.message);
+          error.code = 403;
+          throw error;
+        }
         req.body.user = user;
         next();
       });
     } catch (err) {
       console.log(req.originalUrl, "Authentication failed: ", err.message);
       // TODO: create custom error class to set status code
-      return res.status(403).json({ message: err.message });
+      return res.status(err.code).json({ message: err.message });
     }
   },
   authOptional: async function (req, res, next) {
